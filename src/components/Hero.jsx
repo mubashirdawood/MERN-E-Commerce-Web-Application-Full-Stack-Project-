97e7d5a7e256a9dedc789cf76695dd5e1a50262e
Mubashir Dawood <mubashirdawood05@gmail.com>
2026-04-19 00:40:00 +0500

    single item changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { assets } from "../assets/assets";
 const Hero = () => {
   return (
-    <div className="flex flex-col sm:flex-row border border-b-cyan-950">
+    <div className="flex flex-col sm:flex-row border md:rounded-l-full shadow-2xl shadow-gray-800 border-b-cyan-950">
       {/* hero left */}
       <div className="w-full sm:w-1/2 p-8 flex items-center justify-center py-8 sm:py-0">
         <div className="text-[#414141]">
@@ -10,7 +10,7 @@ const Hero = () => {
             <p className="w-8 md:w-11 h-[2px] bg-[#414141]"></p>
             <p className="font-medium text-sm md:text-base"> OUR BEST SELLER</p>
           </div>
-          <h1 className="prata-regular text-3xl sm:py-3 lg:text-5xl leading-relaxed">
+          <h1 className="prata-regular  animate-bounce text-3xl sm:py-3 lg:text-5xl leading-relaxed">
             Latest Arrivals
           </h1>
           <div className="flex items-center gap-2">
@@ -22,7 +22,7 @@ const Hero = () => {
         </div>
       </div>
       {/* hero right */}
-      <img src={assets.hero_img} className="w-full sm:w-1/2" alt="" />
+      <img src={assets.hero_img} className=" rounded-l-full   shadow-2xl shadow-gray-600 ml-3 sm:w-1/2" alt="" />
     </div>
   );
 };
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -5,40 +5,47 @@ import { ShopContext } from "../context/ShopContext";
 import { useState } from "react";
 import { useEffect } from "react";
 
+const Product = () => {
+  const { productId } = useParams();
+  const { products } = useContext(ShopContext);
+  const [productsData, setProductsData] = useState(false);
+  const [image, setIamage] = useState("");
 
-
-const Product=()=>{
-  const {productId}=useParams();
-  const {products}=useContext(ShopContext);
-  const [productsData,setProductsData]=useState(false);
- const [image,setIamage]=useState("");
-
-  const fetchProduct=()=>{
-
-    products.map((item)=>{
-      if(item._id===productId){
+  const fetchProduct = () => {
+    products.map((item) => {
+      if (item._id === productId) {
         setProductsData(item);
         setIamage(item.image[0]);
         console.log(item);
         return;
       }
-    })
-
-  }
-  useEffect(()=>{
+    });
+  };
+  useEffect(() => {
     fetchProduct();
-  },[productId,products])
+  }, [productId, products]);
   return productsData ? (
     <div className="border-t-2 pt-10 transition-opacity ease-in duration-300 opacity-100">
-   {/* Product Details */}
-  <div className="flex flex-col md:flex-row gap-10 sm:gap-12"></div>
-{/* Product Images */}
+      {/* Product Details */}
+      <div className="flex flex-col md:flex-row gap-10 sm:gap-12"></div>
+      {/* Product Images */}
+      <div className="flex-1 flex flex-col-reverse sm:flex-row gap-3">
+        <div className="flex sm:flex-col  overflow-x-auto sm:overflow-y-auto justify-between sm:justify-normal sm:w-[18.7%] w-full">
+          {
+            productsData.image.map( (item,index)=>{
+              return <img onClick={()=>setIamage(item)} src={item} key={index} className=" w-[24%] sm:w-full sm:mb-3  cursor-pointer" alt="" />;
+            } ) 
+          }
+        </div>
+        <div className="sm:w-[80%] w-full">
+          <img src={image} className="w-full h-auto object-cover" alt="" />
+        </div>
 
-
-
-
+      </div>
     </div>
-  ): <div className="opacity-0"></div>
-}
+  ) : (
+    <div className="opacity-0"></div>
+  );
+};
 
 export default Product;
