967afb874386b3a39c46a120ddf8405f35f19bae
Mubashir Dawood <mubashirdawood05@gmail.com>
2026-04-20 21:49:37 +0500

    home page  changes

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -7,12 +7,12 @@ const ProductItem = ({_id,image,name ,price}) => {
   const {currency}=useContext(ShopContext);
 
   return (
-    <Link className='text-gray cursor-pointer mx-auto' to={`/product/${_id}`}>
+    <Link className='text-gray rounded-2xl cursor-pointer mx-auto' to={`/product/${_id}`}>
       <div className='overflow-hidden'>
-        <img className='hover:scale-150 transition ease-in-out' src={image[0]} alt="" />
+        <img className='hover:scale-150 rounded-2xl border-2 border-gray-500 transition ease-in-out' src={image[0]} alt="" />
       </div>
       <p className='py-3 text-sm'>{name}</p>
-      <p className='font-bold text-lg'> {currency}{price}</p>
+      <p className='font-bold  text-lg '> {currency}{price}</p>
     </Link>
   )
 }
